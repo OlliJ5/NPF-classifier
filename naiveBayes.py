c8c1fdc1671fe84a7d0347fae0f8c57f18e9e99c
Olli Rousu <olli.rousu@hotmail.com>
2021-12-08 10:30:03 +0200

    testing cv on naive bayes

--- naiveBayes.py
+++ naiveBayes.py
@@ -2,32 +2,35 @@ import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
 
 df = pd.read_csv('npf_train.csv')
-print('test')
 
+#Wrangle data
 class2 = np.array([1]*df.shape[0])
 class2[df['class4']=='nonevent'] = 0
-
 df['class2'] = class2
 df = df.drop(['id', 'date', 'class4'], axis=1)
 
-#print(df.head())
 
+#Set X and y
 y = df['class2']
-#print(y)
 X = df.drop(['class2'], axis=1)
-#print(X)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+#train test split
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 
-# print(X_train)
-# print(X_test)
 
-# print(y_train)
-# print(y_test)
-
+#Create model and predict
 gnb = GaussianNB()
-y_pred = gnb.fit(X_train, y_train).predict(X_test)
+scores = cross_val_score(gnb, X, y, cv=5)
 
-print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
+#fitting with basic train/test split
+#y_pred = gnb.fit(X_train, y_train).predict(X_test)
+
+
+#performance measures, accuracy estimations, stats etc.
+print("Scores using cross validation", scores)
+print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+
+#print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
