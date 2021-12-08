6447c9e8ea0d6469796cb04c843962a7ad961b5a
Olli Rousu <olli.rousu@hotmail.com>
2021-12-08 12:33:05 +0200

    confusion matrix when using cv testing

--- naiveBayes.py
+++ naiveBayes.py
@@ -3,6 +3,8 @@ import numpy as np
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import KFold
 
 df = pd.read_csv('npf_train.csv')
 
@@ -23,7 +25,7 @@ X = df.drop(['class2'], axis=1)
 
 #Create model and predict
 gnb = GaussianNB()
-scores = cross_val_score(gnb, X, y, cv=5)
+scores = cross_val_score(gnb, X, y, cv=3)
 
 #fitting with basic train/test split
 #y_pred = gnb.fit(X_train, y_train).predict(X_test)
@@ -31,6 +33,26 @@ scores = cross_val_score(gnb, X, y, cv=5)
 
 #performance measures, accuracy estimations, stats etc.
 print("Scores using cross validation", scores)
+
+#Classification accuracy and standard deviation
 print("%0.2f accuracy with a standard deviation of %0.2f" % (scores.mean(), scores.std()))
+
+#Confusion matrix
+kf = KFold(n_splits=5)
+# for train_index, test_index in kf.split(X):
+
+#    X_train, X_test = X[train_index], X[test_index]
+#    y_train, y_test = y[train_index], y[test_index]
+
+#    gnb.fit(X_train, y_train)
+#    print(confusion_matrix(y_test, gnb.predict(X_test)))
+
+for train_index, test_index in kf.split(X):
+     print("TRAIN:", train_index, "TEST:", test_index)
+     X_train, X_test = X.loc[train_index], X.loc[test_index]
+     y_train, y_test = y[train_index], y[test_index]
+
+     gnb.fit(X_train, y_train)
+     print(confusion_matrix(y_test, gnb.predict(X_test)))
 
 #print("Number of mislabeled points out of a total %d points : %d" % (X_test.shape[0], (y_test != y_pred).sum()))
